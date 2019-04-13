654679bf11dff1cf3d43c7bdb0afc07ab1f6c6f3
mario <910181598@qq.com>
2019-04-14 01:14:00 +0800

    测试/ 更新测试数据，删除了原有数据，使用《Lua程序设计 第二版》的源代码

--- data/lua/luago-book/ch06/sum.lua
+++
@@ -1,7 +0,0 @@
-local sum = 0
-for i = 1, 100 do
-  if i % 2 == 0 then
-    sum = sum + i
-  end
-end
--- print(sum)
--- data/lua/luago-book/ch07/test.lua
+++
@@ -1,5 +0,0 @@
-local t = {"a", "b", "c"}
-t[2] = "B"
-t["foo"] = "Bar"
-local s = t[3] .. t[2] .. t[1] .. t["foo"] .. #t
---print(s)
