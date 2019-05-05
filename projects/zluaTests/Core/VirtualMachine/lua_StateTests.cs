a3c3c0695934261067d9d94a121dbd1f52d5ee93
mario <910181598@qq.com>
2019-05-05 15:25:25 +0800

    添加并通过若干测试

--- projects/zluaTests/Core/VirtualMachine/lua_StateTests.cs
+++ projects/zluaTests/Core/VirtualMachine/lua_StateTests.cs
@@ -1,6 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using zluaTests;
-namespace ZoloLua.Core.VirtualMachine.Tests{
+namespace zlua.Core.VirtualMachine.Tests{
 	[TestClass()]
 	public class lua_StateTests
 	{
@@ -17,15 +17,49 @@ namespace ZoloLua.Core.VirtualMachine.Tests{
 		}
 		
 		[TestMethod()]
-		public void helloworldChunkTest()
+		public void helloWorldChunkTest()
 		{
-			TestTool.t00("string/helloworld/helloworld");
+			TestTool.t00("string/helloWorld/helloWorld");
 		}
 		
 		[TestMethod()]
-		public void helloworldTest()
+		public void helloWorldTest()
 		{
 			TestTool.t01("print(\"Hello World!\")");
+		}
+		
+		[TestMethod()]
+		public void assignChunkTest()
+		{
+			TestTool.t00("string/assign/localAssign");  // SS p68
+			TestTool.t00("string/assign/global");  // SS p70
+		}
+		
+		[TestMethod()]
+		public void assignTest()
+		{
+			TestTool.t01("\nlocal a = 10\nlocal b = a\n");  // SS p68
+			TestTool.t01("\na = 10\nlocal b = a");  // SS p70
+		}
+		
+		[TestMethod()]
+		public void tableChunkTest()
+		{
+			TestTool.t00("string/table/newtable");  // SS p72
+			TestTool.t00("string/table/setlist");  // SS p74
+			TestTool.t00("string/table/settable");  // SS p77
+			TestTool.t00("string/table/settable1");  // SS p78
+			TestTool.t00("string/table/gettable");  // SS p79
+		}
+		
+		[TestMethod()]
+		public void tableTest()
+		{
+			TestTool.t01("local p = {}");  // SS p72
+			TestTool.t01("local p = {1,2}");  // SS p74
+			TestTool.t01("local p = {[\"a\"]=1}");  // SS p77
+			TestTool.t01("\nlocal a = \"a\"\nlocal p = {[a]=1}\n");  // SS p78
+			TestTool.t01("\nlocal p = {[\"a\"]=1}\nlocal b = p[\"a\"]");  // SS p79
 		}
 		
 		[TestMethod()]
@@ -44,26 +78,6 @@ namespace ZoloLua.Core.VirtualMachine.Tests{
 			TestTool.t01("local function f() end\nlocal a,b,c = f(1,2,3,4)");
 			TestTool.t01("local a,b; return 1,a,b");
 			TestTool.t01("local a,b,c,d,e = 100, ...");
-		}
-		
-		[TestMethod()]
-		public void tableChunkTest()
-		{
-			TestTool.t00("string/table/newtable");
-			TestTool.t00("string/table/setlist");
-			TestTool.t00("string/table/settable");
-			TestTool.t00("string/table/settable1");
-			TestTool.t00("string/table/gettable");
-		}
-		
-		[TestMethod()]
-		public void tableTest()
-		{
-			TestTool.t01("local p = {}");
-			TestTool.t01("local p = {1,2}");
-			TestTool.t01("local p = {[\"a\"]=1}");
-			TestTool.t01("\nlocal a = \"a\"\nlocal p = {[a]=1}\n");
-			TestTool.t01("\nlocal p = {[\"a\"]=1}\nlocal b = p[\"a\"]");
 		}
 		
 	}
