ae406432e4aabe05953e64e29d1bd51d5e44ecad
mario <910181598@qq.com>
2019-05-08 17:47:51 +0800

    将dofile移动到lauxlib，创建stdlib

--- projects/zlua/Core/API/lapi.cs
+++ projects/zlua/Core/API/lapi.cs
@@ -1,6 +1,10 @@
 ﻿
 
+using System;
 using System.Diagnostics;
+using Antlr4.Runtime;
+using ZoloLua.Compiler;
+using ZoloLua.Compiler.CodeGenerator;
 using ZoloLua.Core.Lua;
 using ZoloLua.Core.ObjectModel;
 
@@ -380,90 +384,43 @@ namespace ZoloLua.Core.VirtualMachine
         //}
 
         //#endregion Access*方法 从栈获取值
-    }
 
-    //public static class LApi
-    //{
-    //    #region load and call functions (run Lua code)
-    //    public static void LoadFile(this TThread L, string path)
-    //    {
-    //        AntlrInputStream inputStream; //输入流传递给antlr的输入流
-    //        LuaLexer lexer; //*传递给lexer
-    //        CommonTokenStream tokens; //lexer分析好token流
-    //        LuaParser parser; //*由token流生成AST
-    //        //LParser lp = new LParser(); //Listener遍历AST返回Proto
-    //        using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read)) {
-    //            inputStream = new AntlrInputStream(fs);
-    //            lexer = new LuaLexer(inputStream);
-    //            tokens = new CommonTokenStream(lexer);
-    //            parser = new LuaParser(tokens);
-    //            var tree = parser.chunk();
-    //            var walker = new ParseTreeWalker();
-    //            lp.Visit(tree);
-    //        }
-    //        Proto proto = lp.ChunkProto;
-    //        //初始化k
-    //        L.ns = new List<TValue>();
-    //        foreach (var item in lp.ChunkProto.ns) {
-    //            L.ns.Add((TValue)item);
-    //        }
-    //        L.strs = new List<TValue>();
-    //        foreach (var item in lp.ChunkProto.strs) {
-    //            L.strs.Add((TValue)item);
-    //        }
-    //        Closure closure = new LuaClosure(env: (TTable)L.globalsTable, nUpvals: 0, p: proto);
-    //        L.ciStack.Push(new Callinfo()
-    //        {
-    //            funcIndex = L.topIndex,
-    //            topIndex = L.topIndex + proto.MaxStacksize
-    //        });
-    //        L[L.topIndex].Cl = closure;
-    //        L.topIndex = L.topIndex + proto.MaxStacksize;
-    //    }
-    //    public static void LoadString(this TThread L, string luaCode)
-    //    {
-    //        AntlrInputStream inputStream; //输入流传递给antlr的输入流
-    //        inputStream = new AntlrInputStream(luaCode); //知道可以。就行了。
-    //        LuaLexer lexer; //*传递给lexer
-    //        CommonTokenStream tokens; //lexer分析好token流
-    //        LuaParser parser; //*由token流生成AST
-    //        //LParser lp = new LParser(); //Listener遍历AST返回Proto
-    //        lexer = new LuaLexer(inputStream);
-    //        tokens = new CommonTokenStream(lexer);
-    //        parser = new LuaParser(tokens);
-    //        var tree = parser.chunk();
-    //        var walker = new ParseTreeWalker();
-    //        //lp.Visit(tree);
-    //        Proto proto = lp.ChunkProto;
-    //        //初始化k
-    //        L.ns = new List<TValue>();
-    //        foreach (var item in lp.ChunkProto.ns) {
-    //            L.ns.Add((TValue)item);
-    //        }
-    //        L.strs = new List<TValue>();
-    //        foreach (var item in lp.ChunkProto.strs) {
-    //            L.strs.Add((TValue)item);
-    //        }
-    //        Closure closure = new LuaClosure(env: (TTable)L.globalsTable, nUpvals: 0, p: proto);
-    //        L.ciStack.Push(new Callinfo()
-    //        {
-    //            funcIndex = L.topIndex,
-    //            topIndex = L.topIndex + proto.MaxStacksize
-    //        });
-    //        L[L.topIndex].Cl = closure;
-    //        L.topIndex = L.topIndex + proto.MaxStacksize;
-    //    }
-    //    public delegate void CSharpFunction(TThread L);
-    //    #endregion
-    ////}
-}
+        /// <summary>
+        ///     clua是parser或undump，zlua只parse
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name="chunkname"></param>
+        /// <returns></returns>
+        public int lua_load(ICharStream chunk, string chunkname)
+        {
+            // status是parser返回的错误码
+            int status = 0;
 
-/// <summary>
-/// lua标准库，src就十几行，用于定义和加载所有具体的标准库，项目尾声时处理
-/// </summary>
-namespace ZoloLua.Stdlib
-{
-    internal class lualib
-    {
+            if (chunkname == null) chunkname = "?";
+
+            LuaLexer lexer = new LuaLexer(chunk);
+            CommonTokenStream tokenStream = new CommonTokenStream(lexer);
+            LuaParser parser = new LuaParser(tokenStream);
+            LuaParser.ChunkContext tree = parser.chunk();
+            LuaCodeGenerator codeGenerator = new LuaCodeGenerator();
+            codeGenerator.Visit(tree);
+
+            Proto p = codeGenerator.Chunk;
+            Table env = new Table(1, 1);
+            env.luaH_set(new TValue("print")).Cl = new CSharpClosure
+            {
+                f = L =>
+                {
+                    TValue s = L.pop();
+                    Console.WriteLine(s.Str);
+                    return 0;
+                }
+            };
+            LuaClosure cl = new LuaClosure(env, 1, p);
+            push(new TValue(cl));
+            return status;
+        }
     }
 }
+
+
--- projects/zlua/Library/AuxLib/lauxlib.cs
+++ projects/zlua/Library/AuxLib/lauxlib.cs
@@ -1,4 +1,12 @@
-﻿namespace ZoloLua.Core.VirtualMachine
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using Antlr4.Runtime;
+using ZoloLua.Core.ObjectModel;
+using ZoloLua.Core.Undumper;
+
+namespace ZoloLua.Core.VirtualMachine
 {
     public partial class lua_State
     {
@@ -8,11 +16,61 @@
         /// </summary>
         /// <returns></returns>
         /// <remarks>注意这和lua_State的ctor不同，比如要设置g.mainthread=this</remarks>
-        private static lua_State luaL_newstate()
+        public static lua_State luaL_newstate()
         {
             lua_State L = lua_newstate();
             //TODOlua_atpanic(L, &panic);
             return L;
+        }
+
+        // 《Lua设计与实现》p39
+        public void luaL_dofile(string path)
+        {
+            luaL_loadfile(path);
+            lua_pcall(0, LUA_MULTRET, 0);
+        }
+
+        public void luaL_loadfile(string path)
+        {
+            Proto p;
+            if (IsBinaryChunk(path)) {
+                p = lundump.Undump(new FileStream(path, FileMode.Open));
+                Table env = new Table(1, 1);
+                env.luaH_set(new TValue("print")).Cl = new CSharpClosure
+                {
+                    // c print
+                    f = L =>
+                    {
+                        // pop arg
+                        TValue s = L.pop();
+                        Debug.Assert(s.IsString);
+                        Console.WriteLine(s.Str);
+                        // return 0 result
+                        return 0;
+                    }
+                };
+                LuaClosure cl = new LuaClosure(env, 1, p);
+                push(new TValue(cl));
+            } else {
+                lua_load(new AntlrFileStream(path, Encoding.UTF8), $"@{path}");
+            }
+        }
+
+        /// <summary>
+        ///     zlua和clua这点不同，clua的dofile和dostring都会调用lua_load，后者用lookahead判断是否是二进制，再用parser或unudmp
+        ///     zlua的dostring只能parse，不能undump
+        ///     主要是ANTLRStream，太烦了
+        /// </summary>
+        /// <param name="s"></param>
+        public void luaL_dostring(string s)
+        {
+            luaL_loadstring(s);
+            lua_pcall(0, LUA_MULTRET, 0);
+        }
+
+        public void luaL_loadstring(string s)
+        {
+            lua_load(new AntlrInputStream(s), s);
         }
     }
 }
