f331647b30a54efd39ee1ae2050832b6c648f132
Zolo Mario <910181598@qq.com>
2019-05-10 23:33:07 +0800

    重新配置python解释器，提取xml gen

--- tool/lapi/lapi.py
+++ tool/lapi/lapi.py
@@ -58,53 +58,10 @@ code tag会把单词也变成行
 from tool.csharp import method_def
 from tool.test.util import tab, join, read_all, write_all
 
-
-# region xml
-
-def xml_tag(tag, code):
-    '''
-    def para(code):
-    return ['<para>'] + \
-           tab(code) + \
-           ['</para>']
-    :param tag:
-    :param code:
-    :return:
-    '''
-    return ['<{tag}>\n'.format(tag=tag)] + \
-           tab(code) + \
-           ['</{tag}>\n'.format(tag=tag)]
-
-
-def para(code):
-    return xml_tag('para', code)
-
-
-def remarks(code):
-    return xml_tag('remarks', code)
-
-
-def summary(code):
-    return xml_tag('summary', code)
-
-
-def see(link, code: str):
-    '''<see href="http://stackoverflow.com">HERE</see>
-    code单行，返回一个一个元素的list
-    '''
-    return ['<see href="{link}">{code}</see>\n'.format(link=link, code=code)]
-
-
-def xml_doc(code):
-    '''xml doc顶层是tag列表，不过仍然是code
-    为所有xml doc添加前缀的三斜杠
-    '''
-    return ['/// ' + line for line in code]
-
-
-# endregion
-
 # region app
+from tool.xml import see, summary, xml_doc, remarks, para
+
+
 def my_method_def(method):
     return method_def('public', 'void', method, [], [])
 
